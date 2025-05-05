b01d98635ff825612dc1db1f31eda66b4b4f0c53
jayrajshakha07 <jayrajshakha73@gmail.com>
2025-05-05 16:21:18 +0530

    add navigation and safeArea Provider

--- App.tsx
+++ App.tsx
@@ -3,11 +3,14 @@ import React from 'react';
 import queryClient from './android/app/src/config/queryClient';
 import AppNavigator from './android/app/src/navigation/AppNavigator';
 import './global.css';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <AppNavigator />
+      <SafeAreaProvider>
+        <AppNavigator />
+      </SafeAreaProvider>
     </QueryClientProvider>
   );
 }
--- android/app/src/navigation/AppNavigator.tsx
+++ android/app/src/navigation/AppNavigator.tsx
@@ -1,8 +1,47 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {useEffect} from 'react';
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import {useAuthStore} from '../store/authStore';
-import {View, Text} from 'react-native';
 
-const AppNavigator = () => {
+export type RootStackParamList = {
+  Home: undefined;
+  Details: undefined;
+};
+
+const HomeScreen = () => (
+  <SafeAreaView className="flex-1 items-center justify-center bg-white">
+    <Text>Home Screen</Text>
+  </SafeAreaView>
+);
+
+const ProfileScreen = () => (
+  <SafeAreaView className="flex-1 items-center justify-center bg-white">
+    <Text>Profile Screen</Text>
+  </SafeAreaView>
+);
+
+const DetailsScreen = () => (
+  <SafeAreaView className="flex-1 items-center justify-center bg-white">
+    <Text>Details Screen</Text>
+  </SafeAreaView>
+);
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function MainTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function AppNavigator() {
   const {isAuthenticated, initializeAuth} = useAuthStore();
 
   useEffect(() => {
@@ -11,20 +50,16 @@ const AppNavigator = () => {
     };
     initialize();
   }, [initializeAuth]);
-
   return (
-    <View className="bg-red-500">
-      {isAuthenticated ? (
-        <View>
-          <Text>Hello</Text>
-        </View>
-      ) : (
-        <View>
-          <Text className="text-blue-800">Hello</Text>
-        </View>
-      )}
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="MainTabs"
+          component={MainTabs}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-export default AppNavigator;
+}
