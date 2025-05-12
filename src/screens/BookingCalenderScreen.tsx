7e05ef77ceb046b4b92c0cc96622f1ea88e3b231
PandyaPreet <preetpandya456@gmail.com>
2025-05-12 12:00:54 +0530

    fix: some fix

--- src/screens/BookingCalenderScreen.tsx
+++ src/screens/BookingCalenderScreen.tsx
@@ -243,17 +243,6 @@ const BookingCalenderScreen = ({navigation}) => {
     [],
   );
 
-  const renderCustomHorizontalLine = useCallback(({index, borderColor}) => {
-    return (
-      <View
-        key={`horizontal-line-${index}`}
-        style={{
-          borderColor: 'transparent',
-        }}
-      />
-    );
-  }, []);
-
   return (
     <SafeAreaView style={styles.safeArea} edges={[]}>
       <View style={{flex: 1}}>
@@ -327,7 +316,6 @@ const BookingCalenderScreen = ({navigation}) => {
               renderHour={renderHour}
               hourFormat="h:mm a"
               renderEvent={renderEvent}
-              renderCustomHorizontalLine={renderCustomHorizontalLine}
             />
           </ScrollView>
         </CalendarContainer>
